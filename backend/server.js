6382d51258b3b20470eb24f70bb556e17e72ccc3
Danny <dannycoral2@gmail.com>
2026-03-02 10:02:17 -0500

    fix: update CORS allowed origins

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,20 @@ const { startNotificationService } = require('./services/notificationService');
 const app = express();
 
 // Middlewares globales
-app.use(cors({ 
-  origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  'http://localhost:5173',
+  'https://todo-mern-lj1vo962r-dannycorals-projects.vercel.app',
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
